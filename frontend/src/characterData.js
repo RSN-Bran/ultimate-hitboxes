1d808eec2d1fba3c7b70d683996e0a591ac93054
unknown <brandonsultana1@gmail.com>
2020-04-05 11:14:40 -0400

    commit

--- frontend/src/characterData.js
+++ frontend/src/characterData.js
@@ -9,588 +9,656 @@
     name: "Mario",
     value: "mario",
     number: "01",
-    completed: true
+    completed: true,
+    series: "super-mario"
   },
   {
     id: 2,
     name: "Donkey Kong",
     value: "donkey-kong",
     number: "02",
-    completed: true
+    completed: true,
+    series: "donkey-kong"
   },
   {
     id: 3,
     name: "Link",
     value: "link",
     number: "03",
-    completed: false
+    completed: false,
+    series: "legend-of-zelda"
   },
   {
     id: 4,
     name: "Samus",
     value: "samus",
     number: "04",
-    completed: false
+    completed: false,
+    series: "metroid"
   },
   {
     id: 5,
     name: "Dark Samus",
     value: "dark-samus",
     number: "04e",
-    completed: false
+    completed: false,
+    series: "metroid"
   },
   {
     id: 6,
     name: "Yoshi",
     value: "yoshi",
     number: "05",
-    completed: false
+    completed: false,
+    series: "yoshi"
   },
   {
     id: 7,
     name: "Kirby",
     value: "kirby",
     number: "06",
-    completed: false
+    completed: false,
+    series: "kirby"
   },
   {
     id: 8,
     name: "Fox",
     value: "fox",
     number: "07",
-    completed: false
+    completed: false,
+    series: "star-fox"
   },
   {
     id: 9,
     name: "Pikachu",
     value: "pikachu",
     number: "08",
-    completed: false
+    completed: false,
+    series: "pokemon"
   },
   {
     id: 10,
     name: "Luigi",
     value: "luigi",
     number: "09",
-    completed: false
+    completed: false,
+    series: "super-mario"
   },
   {
     id: 11,
     name: "Ness",
     value: "ness",
     number: "10",
-    completed: false
+    completed: false,
+    series: "earthbound"
   },
   {
     id: 12,
     name: "Captain Falcon",
     value: "captain-falcon",
     number: "11",
-    completed: false
+    completed: false,
+    series: "f-zero"
   },
   {
     id: 13,
     name: "Jigglypuff",
     value: "jigglypuff",
     number: "12",
-    completed: false
+    completed: false,
+    series: "pokemon"
   },
   {
     id: 14,
     name: "Peach",
     value: "peach",
     number: "13",
-    completed: false
+    completed: false,
+    series: "super-mario"
   },
   {
     id: 15,
     name: "Daisy",
     value: "daisy",
     number: "13ε",
-    completed: false
+    completed: false,
+    series: "super-mario"
   },
   {
     id: 16,
     name: "Bowser",
     value: "bowser",
     number: "14",
-    completed: false
+    completed: false,
+    series: "super-mario"
   },
   {
     id: 17,
     name: "Ice Climbers",
     value: "ice-climbers",
     number: "15",
-    completed: false
+    completed: false,
+    series: "ice-climber"
   },
   {
     id: 18,
     name: "Sheik",
     value: "sheik",
     number: "16",
-    completed: false
+    completed: false,
+    series: "legend-of-zelda"
   },
   {
     id: 19,
     name: "Zelda",
     value: "zelda",
     number: "17",
-    completed: false
+    completed: false,
+    series: "legend-of-zelda"
   },
   {
     id: 20,
     name: "Dr. Mario",
     value: "dr-mario",
     number: "18",
-    completed: false
+    completed: false,
+    series: "super-mario"
   },
   {
     id: 21,
     name: "Pichu",
     value: "pichu",
     number: "19",
-    completed: false
+    completed: false,
+    series: "pokemon"
   },
   {
     id: 22,
     name: "Falco",
     value: "falco",
     number: "20",
-    completed: false
+    completed: false,
+    series: "star-fox"
   },
   {
     id: 23,
     name: "Marth",
     value: "marth",
     number: "21",
-    completed: false
+    completed: false,
+    series: "fire-emblem"
   },
   {
     id: 24,
     name: "Lucina",
     value: "lucina",
     number: "21ε",
-    completed: false
+    completed: false,
+    series: "fire-emblem"
   },
   {
     id: 25,
     name: "Young Link",
     value: "young-link",
     number: "22",
-    completed: false
+    completed: false,
+    series: "legend-of-zelda"
   },
   {
     id: 26,
     name: "Ganondorf",
     value: "ganondorf",
     number: "23",
-    completed: false
+    completed: false,
+    series: "legend-of-zelda"
   },
   {
     id: 27,
     name: "Mewtwo",
     value: "mewtwo",
     number: "24",
-    completed: false
+    completed: false,
+    series: "pokemon"
   },
   {
     id: 28,
     name: "Roy",
     value: "roy",
     number: "25",
-    completed: false
+    completed: false,
+    series: "fire-emblem"
   },
   {
     id: 29,
     name: "Chrom",
     value: "chrom",
     number: "25ε",
-    completed: false
+    completed: false,
+    series: "fire-emblem"
   },
   {
     id: 30,
     name: "Mr. Game & Watch",
     value: "mr-game-watch",
     number: "26",
-    completed: false
+    completed: false,
+    series: "game-watch"
   },
   {
     id: 31,
     name: "Meta Knight",
     value: "meta-knight",
     number: "27",
-    completed: false
+    completed: false,
+    series: "kirby"
   },
   {
     id: 32,
     name: "Pit",
     value: "pit",
     number: "28",
-    completed: false
+    completed: false,
+    series: "kid-icarus"
   },
   {
     id: 33,
     name: "Dark Pit",
     value: "dark-pit",
     number: "28ε",
-    completed: false
+    completed: false,
+    series: "kid-icarus"
   },
   {
     id: 34,
     name: "Zero Suit Samus",
     value: "zero-suit-samus",
     number: "29",
-    completed: false
+    completed: false,
+    series: "metroid"
   },
   {
     id: 35,
     name: "Wario",
     value: "wario",
     number: "30",
-    completed: false
+    completed: false,
+    series: "wario"
   },
   {
     id: 36,
     name: "Snake",
     value: "snake",
     number: "31",
-    completed: false
+    completed: false,
+    series: "metal-gear"
   },
   {
     id: 37,
     name: "Ike",
     value: "ike",
     number: "32",
-    completed: false
+    completed: false,
+    series: "fire-emblem"
   },
   {
     id: 38,
     name: "Squirtle",
     value: "squirtle",
     number: "33",
-    completed: false
+    completed: false,
+    series: "pokemon"
   },
   {
     id: 39,
     name: "Ivysaur",
     value: "ivysaur",
     number: "34",
-    completed: false
+    completed: false,
+    series: "pokemon"
   },
   {
     id: 40,
     name: "Charizard",
     value: "charizard",
     number: "35",
-    completed: false
+    completed: false,
+    series: "pokemon"
   },
   {
     id: 40,
     name: "Diddy Kong",
     value: "diddy-kong",
     number: "36",
-    completed: false
+    completed: false,
+    series: "donkey-kong"
   },
   {
     id: 41,
     name: "Lucas",
     value: "lucas",
     number: "37",
-    completed: false
+    completed: false,
+    series: "earthbound"
   },
   {
     id: 42,
     name: "Sonic",
     value: "sonic",
     number: "38",
-    completed: false
+    completed: false,
+    series: "sonic"
   },
   {
     id: 43,
     name: "King Dedede",
     value: "king-dedede",
     number: "39",
-    completed: false
+    completed: false,
+    series: "kirby"
   },
   {
     id: 44,
     name: "Olimar",
     value: "olimar",
     number: "40",
-    completed: false
+    completed: false,
+    series: "pikmin"
   },
   {
     id: 45,
     name: "Lucario",
     value: "lucario",
     number: "41",
-    completed: false
+    completed: false,
+    series: "pokemon"
   },
   {
     id: 46,
     name: "R.O.B.",
     value: "rob",
     number: "42",
-    completed: false
+    completed: false,
+    series: "rob"
   },
   {
     id: 47,
     name: "Toon Link",
     value: "toon-link",
     number: "43",
-    completed: false
+    completed: false,
+    series: "legend-of-zelda"
   },
   {
     id: 48,
     name: "Wolf",
     value: "wolf",
     number: "44",
-    completed: false
+    completed: false,
+    series: "star-fox"
   },
   {
     id: 49,
     name: "Villager",
     value: "villager",
     number: "45",
-    completed: false
+    completed: false,
+    series: "animal-crossing"
   },
   {
     id: 50,
     name: "Mega Man",
     value: "mega-man",
     number: "46",
-    completed: false
+    completed: false,
+    series: "mega-man"
   },
   {
     id: 51,
     name: "Wii Fit Trainer",
     value: "wii-fit-trainer",
     number: "47",
-    completed: false
+    completed: false,
+    series: "wii-fit"
   },
   {
     id: 52,
     name: "Rosalina & Luma",
     value: "rosalina-luma",
     number: "48",
-    completed: false
+    completed: false,
+    series: "super-mario"
   },
   {
     id: 53,
     name: "Little Mac",
     value: "little-mac",
     number: "49",
-    completed: false
+    completed: false,
+    series: "punch-out"
   },
   {
     id: 54,
     name: "Greninja",
     value: "greninja",
     number: "50",
-    completed: false
+    completed: false,
+    series: "pokemon"
   },
   {
     id: 55,
     name: "Mii Brawler",
     value: "mii-brawler",
     number: "51",
-    completed: false
+    completed: false,
+    series: "smash-bros"
   },
   {
     id: 56,
     name: "Mii Swordfighter",
     value: "mii-swordfighter",
     number: "52",
-    completed: false
+    completed: false,
+    series: "smash-bros"
   },
   {
     id: 57,
     name: "Mii Gunner",
     value: "mii-gunner",
     number: "53",
-    completed: false
+    completed: false,
+    series: "smash-bros"
   },
   {
     id: 58,
     name: "Palutena",
     value: "palutena",
     number: "54",
-    completed: false
+    completed: false,
+    series: "kid-icarus"
   },
   {
     id: 59,
     name: "Pac-Man",
     value: "pac-man",
     number: "55",
-    completed: false
-  },
-  {
-    id: 59,
-    name: "Pac-Man",
-    value: "pac-man",
-    number: "55",
-    completed: false
+    completed: false,
+    series: "pac-man"
   },
   {
     id: 60,
     name: "Robin",
     value: "robin",
     number: "56",
-    completed: false
+    completed: false,
+    series: "fire-emblem"
   },
   {
     id: 61,
     name: "Shulk",
     value: "shulk",
     number: "57",
-    completed: false
+    completed: false,
+    series: "xenoblade"
   },
   {
     id: 62,
     name: "Bowser Jr.",
     value: "bowser-jr",
     number: "58",
-    completed: false
+    completed: false,
+    series: "super-mario"
   },
   {
     id: 63,
     name: "Duck Hunt",
     value: "duck-hunt",
     number: "59",
-    completed: false
+    completed: false,
+    series: "duck-hunt"
   },
   {
     id: 64,
     name: "Ryu",
     value: "ryu",
     number: "60",
-    completed: false
+    completed: false,
+    series: "street-fighter"
   },
   {
     id: 65,
     name: "Ken",
     value: "ken",
     number: "60ε",
-    completed: false
+    completed: false,
+    series: "street-fighter"
   },
   {
     id: 66,
     name: "Cloud",
     value: "cloud",
     number: "61",
-    completed: false
+    completed: false,
+    series: "final-fantasy"
   },
   {
     id: 67,
     name: "Corrin",
     value: "corrin",
     number: "62",
-    completed: false
+    completed: false,
+    series: "fire-emblem"
   },
   {
     id: 68,
     name: "Bayonetta",
     value: "bayonetta",
     number: "63",
-    completed: false
-  },
-  {
-    id: 70,
-    name: "Bayonetta",
-    value: "bayonetta",
-    number: "63",
-    completed: false
+    completed: false,
+    series: "bayonetta"
   },
   {
     id: 71,
     name: "Inkling",
     value: "inkling",
     number: "64",
-    completed: false
+    completed: false,
+    series: "splatoon"
   },
   {
     id: 72,
     name: "Ridley",
     value: "ridley",
     number: "65",
-    completed: false
+    completed: false,
+    series: "metroid"
   },
   {
     id: 73,
     name: "Simon",
     value: "simon",
     number: "66",
-    completed: false
+    completed: false,
+    series: "castlevania"
   },
   {
     id: 74,
     name: "Richter",
     value: "richter",
     number: "66ε",
-    completed: false
+    completed: false,
+    series: "castlevania"
   },
   {
     id: 75,
     name: "King K. Rool",
     value: "king-k-rool",
     number: "67",
-    completed: false
+    completed: false,
+    series: "donkey-kong"
   },
   {
     id: 76,
     name: "Isabelle",
     value: "isabelle",
     number: "68",
-    completed: false
+    completed: false,
+    series: "animal-crossing"
   },
   {
     id: 77,
     name: "Incineroar",
     value: "incineroar",
     number: "69",
-    completed: false
+    completed: false,
+    series: "pokemon"
   },
   {
     id: 78,
     name: "Piranha Plant",
     value: "piranha-plant",
     number: "70",
-    completed: false
+    completed: false,
+    series: "super-mario"
   },
   {
     id: 79,
     name: "Joker",
     value: "joker",
     number: "71",
-    completed: false
+    completed: false,
+    series: "persona"
   },
   {
     id: 80,
     name: "Hero",
     value: "hero",
     number: "72",
-    completed: false
+    completed: false,
+    series: "dragon-quest"
   },
   {
     id: 81,
     name: "Banjo & Kazooie",
     value: "banjo-kazooie",
     number: "73",
-    completed: false
+    completed: false,
+    series: "banjo-kazooie"
   },
   {
     id: 82,
     name: "Terry",
     value: "terry",
     number: "74",
-    completed: false
+    completed: false,
+    series: "fatal-fury"
   },
   {
     id: 82,
     name: "Byleth",
     value: "byleth",
     number: "75",
-    completed: false
+    completed: false,
+    series: "fire-emblem"
   },
 ]
 
--- frontend/src/components/Character.js
+++ frontend/src/components/Character.js
@@ -5,12 +5,14 @@ import '../css/Character.css';
 function Character(props) {
 
 	let renderURL = "https://ultimate-hitboxes.s3.amazonaws.com/characters/" + props.character.value + ".png"
+	let seriesURL = "https://ultimate-hitboxes.s3.amazonaws.com/series-symbol/" + props.character.series + ".png"
 	if (props.character.completed) {
 		return (
 			<div className="character" value={props.character.value} onClick={props.getCharacterData.bind(this, props.character.value)}>
 				<small style={{ "position": "absolute" }}>#{props.character.number.replace('e', 'ε')}</small>
 				<h2 style={{ "position": "absolute" }}>{props.character.name}</h2>
 				<img src={renderURL} height="100" alt={props.character.name}></img>
+				<img className="series-icon" src={seriesURL} alt={props.character.series}></img>
 			</div>
 		)
 	}
@@ -20,6 +22,7 @@ function Character(props) {
 				<small style={{ "position": "absolute" }}>#{props.character.number.replace('e', 'ε')}</small>
 				<h2 style={{ "position": "absolute" }}>{props.character.name}</h2>
 				<img src={renderURL} height="100" alt={props.character.name}></img>
+				<img className="series-icon" src={seriesURL} alt={props.character.series}></img>
 			</div>
 		)
 	}

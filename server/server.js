9aba9b18205d1bff78d2ab26624abfeea405c130
unknown <brandonsultana1@gmail.com>
2020-07-03 14:59:32 -0400

    test watch 2

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ app.get('/:character/:move/data', (req, res) => {
 
 app.get('/no', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.send("nooooooo") 
+  res.send("noooooooOOOOOOOO") 
 });
 
 // console.log that your server is up and running

60d9423ea317ebabc67255129378f6b9cbad4d47
unknown <brandonsultana1@gmail.com>
2020-07-12 00:29:28 -0400

    Show current version of the character

--- frontend/src/components/Character.js
+++ frontend/src/components/Character.js
@@ -12,7 +12,8 @@ function Character(props) {
 	//Associated class varies based on if the character has completed data.Incomplete characters are not selectable and grayed out
 	return (
 		<div className={props.character.completed ? "character" : "character-incomplete"} value={props.character.value} onClick={props.getCharacterData.bind(this, props.character.value)}>
-			<small style={{ "position": "absolute" }}>#{props.character.number.replace('e', 'ε')}</small>
+			<small className= "number" style={{ "position": "absolute" }}>#{props.character.number.replace('e', 'ε')}</small>
+			<small className= "version" style={{ "position": "absolute" }}>{props.character.completed ? "v" + props.character.version : "Coming Soon"}</small>
 			<h2 style={{ "position": "absolute" }}>{props.character.name}</h2>
 			<img src={renderURL} height="100" alt={props.character.name}></img>
 			<img className="series-icon" src={seriesURL} alt={props.character.series}></img>

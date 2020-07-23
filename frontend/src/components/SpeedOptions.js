7059d8de6a60f3323a7f15b00342825686af138e
unknown <brandonsultana1@gmail.com>
2020-07-22 20:56:10 -0400

    CSS Updates + Meta Knight

--- frontend/src/components/SpeedOptions.js
+++ frontend/src/components/SpeedOptions.js
@@ -10,32 +10,32 @@ function SpeedOptions(props) {
 				<h5>Play Speed: </h5>
 				<div className="speedButton" id="hundrethSpeed">
 					<input type="radio" name="playSpeed" value="60" onChange={props.changeSpeed} checked={props.playSpeed == 60} />
-					<label htmlFor="1fps">1fps</label>
+					<label className="speedLabel" htmlFor="1fps">1fps</label>
 				</div>
 
 				<div className="speedButton" id="tenthSpeed">
 					<input type="radio" name="playSpeed" value="10" onChange={props.changeSpeed} checked={props.playSpeed == 10} />
-					<label htmlFor=".1x">.1x</label>
+					<label className="speedLabel" htmlFor=".1x">.1x</label>
 				</div>
 
 				<div className="speedButton" id="quarterSpeed">
 					<input type="radio" name="playSpeed" value="4" onChange={props.changeSpeed} checked={props.playSpeed == 4} />
-					<label htmlFor=".25x">.25x</label>
+					<label className="speedLabel" htmlFor=".25x">.25x</label>
 				</div>
 
 				<div className="speedButton" id="halfSpeed">
 					<input type="radio" name="playSpeed" value="2" onChange={props.changeSpeed} checked={props.playSpeed == 2} />
-					<label htmlFor=".5x">.5x</label>
+					<label className="speedLabel" htmlFor=".5x">.5x</label>
 				</div>
 
 				<div className="speedButton" id="baseSpeed">
 					<input type="radio" name="playSpeed" value="1" onChange={props.changeSpeed} checked={props.playSpeed == 1} />
-					<label htmlFor="1x">1x</label>
+					<label className="speedLabel" htmlFor="1x">1x</label>
 				</div>
 
 				<div className="speedButton" id="doubleSpeed">
 					<input type="radio" name="playSpeed" value=".5" onChange={props.changeSpeed} checked={props.playSpeed == .5} />
-					<label htmlFor="2x">2x</label>
+					<label className="speedLabel" htmlFor="2x">2x</label>
 				</div>
 			</div>
 		)

56cc9a030a9bd985edcdee9338f2ad777631b8a1
unknown <brandonsultana1@gmail.com>
2020-08-03 00:33:50 -0400

    More mobile adjustments

--- frontend/src/components/Info.js
+++ frontend/src/components/Info.js
@@ -10,13 +10,15 @@ function Info(props) {
 	if (props.info) {
 		return (
 			<div id="infoText">
-				<br/>
-				All code written and hosted by Brandon "<a href="https://twitter.com/RSN_Bran">RSN_Bran</a>" Sultana.
-		<br />
-				Hitbox image credits to <a href="https://twitter.com/Zeckemyro">Zeckemyro</a>. Images retreived from <a href="https://drive.google.com/drive/folders/1IvQvVTNIvvPeGzNCRIRnvOsVKQs1FgVR">here</a>.
-		<br />
-				Hitbox data credits to <a href="https://twitter.com/Ruben_dal">Ruben</a>. Data retreived from <a href="https://rubendal.github.io/ssbu/#/">here</a>.
-
+				<div className = "info">
+					All code written and hosted by Brandon "<a href="https://twitter.com/RSN_Bran">RSN_Bran</a>" Sultana.
+				</div>
+				<div className="info">
+					Hitbox image credits to <a href="https://twitter.com/Zeckemyro">Zeckemyro</a>. Images retreived from <a href="https://drive.google.com/drive/folders/1IvQvVTNIvvPeGzNCRIRnvOsVKQs1FgVR">here</a>.
+				</div>
+				<div className="info">
+					Hitbox data credits to <a href="https://twitter.com/Ruben_dal">Ruben</a>. Data retreived from <a href="https://rubendal.github.io/ssbu/#/">here</a>.
+				</div>
 				<img id="exitInfo" onClick={props.showInfo} src={x} />
 		</div>
 		)

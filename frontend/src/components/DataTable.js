3d79b2511842d0a6d9d715776c481a2b910393d8
unknown <brandonsultana1@gmail.com>
2020-08-15 12:33:46 -0400

    Added Charizard, more CSS fixes

--- frontend/src/components/DataTable.js
+++ frontend/src/components/DataTable.js
@@ -5,63 +5,81 @@ import HitboxTable from './HitboxTable'
 import '../css/DataTable.css';
 
 function DataTable(props) {
+
+  let frames = { "variable": "frames", "name": "Frame", "toolTipID": "frameToolTip", "toolTipDescription": "First Active Frame of the hitbox.Click on the number to jump to that frame." }
+  let size = { "variable": "size", "name": "Size", "toolTipID": "sizeToolTip", "toolTipDescription": "Size of the hitbox." }
+  let ground_or_air = { "variable": "ground_or_air", "name": "Ground/Air", "toolTipID": "ground-airToolTip", "toolTipDescription": "Determines if the hitbox affects grounded or aerial opponents" }
+  let more = { "variable": "more", "name": "More Data", "toolTipID": "moreToolTip", "toolTipDescription": "Shows all raw data for a hitbox" }
+  let id = { "variable": "id", "name": "ID", "toolTipID": "idToolTip", "toolTipDescription": "ID of the Hitbox" }
+  let bone = { "variable": "bone", "name": "Bone", "toolTipID": "boneToolTip", "toolTipDescription": "Bone of the model the Hitbox is attached to" }
+  let x = { "variable": "x", "name": "X", "toolTipID": "xToolTip", "toolTipDescription": "X Position of the hitbox in relation to the center of the bone" }
+  let y = { "variable": "y", "name": "Y", "toolTipID": "yToolTip", "toolTipDescription": "Y Position of the hitbox in relation to the center of the bone" }
+  let z = { "variable": "z", "name": "Z", "toolTipID": "zToolTip", "toolTipDescription": "Z Position of the hitbox in relation to the center of the bone" }
+  let damage = { "variable": "damage", "name": "Dmg", "toolTipID": "dmgToolTip", "toolTipDescription": "Damage dealt by the hitbox" }
+  let shielddamage = { "variable": "shielddamage", "name": "SD", "toolTipID": "sdToolTip", "toolTipDescription": "Shield Damage" }
+  let angle = { "variable": "angle", "name": "Angle", "toolTipID": "angleToolTip", "toolTipDescription": "Launch Angle, 361 denotes the \"Sakurai Angle\"" }
+  let bkb = { "variable": "bkb", "name": "BKB", "toolTipID": "bkbToolTip", "toolTipDescription": "Base Knockback" }
+  let kbg = { "variable": "kbg", "name": "KBG", "toolTipID": "kbgToolTip", "toolTipDescription": "Knockback Growth" }
+  let fkb = { "variable": "fkb", "name": "FKB", "toolTipID": "fkbToolTip", "toolTipDescription": "Fixed Knockback" }
+  let trip = { "variable": "trip", "name": "Trip", "toolTipID": "tripToolTip", "toolTipDescription": "Trip Chance" }
+  let sdi = { "variable": "sdi", "name": "SDI", "toolTipID": "sdiToolTip", "toolTipDescription": "How much each SDI input affects the victim's position" }
+
   //Only render when needed
   if (props.portalState === "hasMove" && !props.pickingCharacter) {
-    let returnTable;
     let fields = [];
     if (props.move.type === "grab" && !props.showExtraInfo) {
       fields = [
-        { "variable": "frames", "name": "Frame", "toolTipID": "frameToolTip", "toolTipDescription": "First Active Frame of the hitbox.Click on the number to jump to that frame." },
-        { "variable": "size", "name": "Size", "toolTipID": "sizeToolTip", "toolTipDescription": "Size of the hitbox." },
-        { "variable": "ground_or_air", "name": "Ground/Air", "toolTipID": "ground-airToolTip", "toolTipDescription": "Determines if the hitbox affects grounded or aerial opponents" },
-        { "variable": "more", "name": "More Data", "toolTipID": "moreToolTip", "toolTipDescription": "Shows all raw data for a hitbox" },
+        frames,
+        size,
+        ground_or_air,
+        more
       ]
     }
     else if (props.move.type === "grab" && props.showExtraInfo) {
       fields = [
-        { "variable": "id", "name": "ID", "toolTipID": "idToolTip", "toolTipDescription": "ID of the Hitbox" },
-        { "variable": "frames", "name": "Frame", "toolTipID": "frameToolTip", "toolTipDescription": "First Active Frame of the hitbox.Click on the number to jump to that frame." },
-        { "variable": "size", "name": "Size", "toolTipID": "sizeToolTip", "toolTipDescription": "Size of the hitbox." },
-        { "variable": "ground_or_air", "name": "Ground/Air", "toolTipID": "ground-airToolTip", "toolTipDescription": "Determines if the hitbox affects grounded or aerial opponents" },
-        { "variable": "bone", "name": "Bone", "toolTipID": "boneToolTip", "toolTipDescription": "Bone of the model the Hitbox is attached to" },
-        { "variable": "x", "name": "X", "toolTipID": "xToolTip", "toolTipDescription": "X Position of the hitbox in relation to the center of the bone" },
-        { "variable": "y", "name": "Y", "toolTipID": "yToolTip", "toolTipDescription": "Y Position of the hitbox in relation to the center of the bone" },
-        { "variable": "z", "name": "Z", "toolTipID": "zToolTip", "toolTipDescription": "Z Position of the hitbox in relation to the center of the bone" },
-        { "variable": "more", "name": "More Data", "toolTipID": "moreToolTip", "toolTipDescription": "Shows all raw data for a hitbox" },
+        id,
+        frames,
+        size,
+        ground_or_air,
+        bone,
+        x,
+        y,
+        z,
+        more
       ]
     }
     else if (props.move.type !== "grab" && !props.showExtraInfo) {
       fields = [
-        { "variable": "frames", "name": "Frame", "toolTipID": "frameToolTip", "toolTipDescription": "First Active Frame of the hitbox.Click on the number to jump to that frame." },
-        { "variable": "damage", "name": "Dmg", "toolTipID": "dmgToolTip", "toolTipDescription": "Damage dealt by the hitbox" },
-        { "variable": "shielddamage", "name": "SD", "toolTipID": "sdToolTip", "toolTipDescription": "Shield Damage" },
-        { "variable": "angle", "name": "Angle", "toolTipID": "angleToolTip", "toolTipDescription": "Launch Angle, 361 denotes the \"Sakurai Angle\"" },
-        { "variable": "bkb", "name": "BKB", "toolTipID": "bkbToolTip", "toolTipDescription": "Base Knockback" },
-        { "variable": "kbg", "name": "KBG", "toolTipID": "kbgToolTip", "toolTipDescription": "Knockback Growth" },
-        { "variable": "fkb", "name": "FKB", "toolTipID": "fkbToolTip", "toolTipDescription": "Fixed Knockback" },
-        { "variable": "trip", "name": "Trip", "toolTipID": "tripToolTip", "toolTipDescription": "Trip Chance" },
-        { "variable": "more", "name": "More Data", "toolTipID": "moreToolTip", "toolTipDescription": "Shows all raw data for a hitbox" },
+        frames,
+        damage,
+        shielddamage,
+        angle,
+        bkb,
+        kbg,
+        fkb,
+        trip,
+        more,
       ]
     }
     else if (props.move.type !== "grab" && props.showExtraInfo) {
       fields = [
-        { "variable": "id", "name": "ID", "toolTipID": "idToolTip", "toolTipDescription": "ID of the Hitbox" },
-        { "variable": "frames", "name": "Frame", "toolTipID": "frameToolTip", "toolTipDescription": "First Active Frame of the hitbox.Click on the number to jump to that frame." },
-        { "variable": "damage", "name": "Dmg", "toolTipID": "dmgToolTip", "toolTipDescription": "Damage dealt by the hitbox" },
-        { "variable": "shielddamage", "name": "SD", "toolTipID": "sdToolTip", "toolTipDescription": "Shield Damage" },
-        { "variable": "angle", "name": "Angle", "toolTipID": "angleToolTip", "toolTipDescription": "Launch Angle, 361 denotes the \"Sakurai Angle\"" },
-        { "variable": "bkb", "name": "BKB", "toolTipID": "bkbToolTip", "toolTipDescription": "Base Knockback" },
-        { "variable": "kbg", "name": "KBG", "toolTipID": "kbgToolTip", "toolTipDescription": "Knockback Growth" },
-        { "variable": "fkb", "name": "FKB", "toolTipID": "fkbToolTip", "toolTipDescription": "Fixed Knockback" },
-        { "variable": "trip", "name": "Trip", "toolTipID": "tripToolTip", "toolTipDescription": "Trip Chance" },
-        { "variable": "sdi", "name": "SDI", "toolTipID": "sdiToolTip", "toolTipDescription": "How much each SDI input affects the victim's position" },
-        { "variable": "ground_or_air", "name": "Ground/Air", "toolTipID": "ground-airToolTip", "toolTipDescription": "Determines if the hitbox affects grounded or aerial opponents" },
-        { "variable": "size", "name": "Size", "toolTipID": "sizeToolTip", "toolTipDescription": "Size of the hitbox." },
-        { "variable": "bone", "name": "Bone", "toolTipID": "boneToolTip", "toolTipDescription": "Bone of the model the Hitbox is attached to" },
-        { "variable": "x", "name": "X", "toolTipID": "xToolTip", "toolTipDescription": "X Position of the hitbox in relation to the center of the bone" },
-        { "variable": "y", "name": "Y", "toolTipID": "yToolTip", "toolTipDescription": "Y Position of the hitbox in relation to the center of the bone" },
-        { "variable": "z", "name": "Z", "toolTipID": "zToolTip", "toolTipDescription": "Z Position of the hitbox in relation to the center of the bone" },
-        { "variable": "more", "name": "More Data", "toolTipID": "moreToolTip", "toolTipDescription": "Shows all raw data for a hitbox" },
+        id,
+        frames,
+        damage,
+        shielddamage,
+        angle,
+        bkb,
+        kbg,
+        fkb,
+        trip,
+        sdi,
+        ground_or_air,
+        size,
+        bone,
+        x,
+        y,
+        z,
+        more,
       ]
     }
 
--- frontend/src/components/HitboxEntry.js
+++ frontend/src/components/HitboxEntry.js
@@ -7,13 +7,14 @@ import id_colors from '../id_colors.js'
 function HitboxEntry(props) {
 
   let style = {}
+  let className = props.dark_light === 0 ? "darkTable" : "lightTable"
+
+  let lightModeColorChange = ["#800080", "#400040", "purple"]
+
   if (props.hitbox.frames.includes(props.currentFrame) || props.hitbox.frames.length === 0) {
     style.backgroundColor = props.hitbox.color
-    if (props.hitbox.color === "yellow" || props.hitbox.color === "white") {
-      style.color = "black"
-    }
-    else {
-      style.color = "white"
+    if (props.dark_light === 1 && lightModeColorChange.includes(props.hitbox.color)) {
+      className = "lightTableLightText"
     }
   }
 
@@ -22,8 +23,8 @@ function HitboxEntry(props) {
     //If adding the frames variable, only show the first frame in the table
     if (field.variable === "frames") {
       tdList.push(<td
-        className={props.dark_light === 0 ? "darkTable" : "lightTable"}
-        style={{ "cursor": "pointer" }}
+        className={className}
+        style={props.hitbox.frames.length !== 0 ? { "cursor": "pointer" } : {}}
         onClick={props.jumpToFrame.bind(this, props.hitbox.frames[0])}>
         {props.hitbox[field.variable][0]}
       </td>)
@@ -33,21 +34,21 @@ function HitboxEntry(props) {
     else if (field.variable === "ground_or_air") {
       let ground_air = { "collision_situation_mask_g": "Ground", "collision_situation_mask_a": "Aerial", "collision_situation_mask_ga": "Both"};
 
-      tdList.push(<td className={props.dark_light === 0 ? "darkTable" : "lightTable"}>{ground_air[props.hitbox[field.variable]] === undefined ? "-" : ground_air[props.hitbox[field.variable]]}</td>)
+      tdList.push(<td className={className}>{ground_air[props.hitbox[field.variable]] === undefined ? "-" : ground_air[props.hitbox[field.variable]]}</td>)
     }
 
     //Add Degree symbol after the angle
     else if (field.variable === "angle") {
-      tdList.push(<td className={props.dark_light === 0 ? "darkTable" : "lightTable"}>{props.hitbox[field.variable]}&deg;</td>)
+      tdList.push(<td className={className}>{props.hitbox[field.variable]}&deg;</td>)
     }
 
     //If showing more data, create a button to click in the table
     else if (field.variable === "more") {
-      tdList.push(<td className={props.dark_light === 0 ? "darkTable" : "lightTable"}><button onClick={props.updateHitboxData.bind(this, props.hitbox)} src="moreInfo"> More Data </button></td >)
+      tdList.push(<td className={className}><button onClick={props.updateHitboxData.bind(this, props.hitbox)} src="moreInfo"> More Data </button></td >)
     }
 
     else {
-      tdList.push(<td className={props.dark_light === 0 ? "darkTable" : "lightTable"}>{props.hitbox[field.variable] === undefined ? "-" : props.hitbox[field.variable]}</td>)
+      tdList.push(<td className={className}>{props.hitbox[field.variable] === undefined ? "-" : props.hitbox[field.variable]}</td>)
     }
   })
   return (

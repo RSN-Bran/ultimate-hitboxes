e2246adf9d98b5e9b3604ff1772206383243ec10
Brandon Sultana <brandonsultana1@gmail.com>
2021-07-03 09:48:27 -0400

    donate

--- frontend/src/components/Info.js
+++ frontend/src/components/Info.js
@@ -13,26 +13,45 @@ function Info(props) {
 	
 		return (
 			<div id="infoText">
-				<div className="info">
-					<h3>Credits</h3>
+				<div className="infoSection">
+					<div className="info">
+						<h3>Credits</h3>
+					</div>
+					<div className = "info">
+						All code written and hosted by Brandon "<a href="https://twitter.com/RSN_Bran">RSN_Bran</a>" Sultana.
+					</div>
+					<div className="info">
+						Hitbox image credits to <a href="https://twitter.com/Zeckemyro">Zeckemyro</a>. Images retreived from <a href="https://drive.google.com/drive/folders/1IvQvVTNIvvPeGzNCRIRnvOsVKQs1FgVR">here</a>.
+					</div>
+					<div className="info">
+						Hitbox data credits to <a href="https://twitter.com/Ruben_dal">Ruben</a>. Data retreived from <a href="https://rubendal.github.io/ssbu/#/">here</a>.
+					</div>
+					<div className="info">
+						Fact checking and notes from <a href="https://ultimateframedata.com/">Ultimate Frame Data</a>
+					</div>
+					<div className="info">
+						Website concept inspired by <a href="https://struz.github.io/smash-move-viewer/#/v1">Smash 4 Move Viewer</a> by <a href="https://twitter.com/StruzSmash">Strutz</a>.
+					</div>
 				</div>
-				<div className = "info">
-					All code written and hosted by Brandon "<a href="https://twitter.com/RSN_Bran">RSN_Bran</a>" Sultana.
+
+				<div className="infoSection">
+					<div className="info">
+						<h3>Donate</h3>
+					</div>
+					<div className="info">
+						Donate here if you would like, all donations go into keeping the site up and running
+					</div>
+					<form id="donate" action="https://www.paypal.com/donate" method="post" target="_top">
+						<input type="hidden" name="business" value="ZY4J7NP8QZMUC" />
+						<input type="hidden" name="no_recurring" value="0" />
+						<input type="hidden" name="currency_code" value="USD" />
+						<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+						<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+					</form>
 				</div>
-				<div className="info">
-					Hitbox image credits to <a href="https://twitter.com/Zeckemyro">Zeckemyro</a>. Images retreived from <a href="https://drive.google.com/drive/folders/1IvQvVTNIvvPeGzNCRIRnvOsVKQs1FgVR">here</a>.
-				</div>
-				<div className="info">
-					Hitbox data credits to <a href="https://twitter.com/Ruben_dal">Ruben</a>. Data retreived from <a href="https://rubendal.github.io/ssbu/#/">here</a>.
-				</div>
-				<div className="info">
-					Fact checking and notes from <a href="https://ultimateframedata.com/">Ultimate Frame Data</a>
-				</div>
-				<div className="info">
-					Website concept inspired by <a href="https://struz.github.io/smash-move-viewer/#/v1">Smash 4 Move Viewer</a> by <a href="https://twitter.com/StruzSmash">Strutz</a>.
-				</div>
-				
-		</div>
+			</div>
+
+
 		)
 	
 }

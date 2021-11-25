5d05f5d7d81633da97fa53d63e1440c827df2318
Brandon Sultana <brandonsultana1@gmail.com>
2021-11-25 08:59:24 -0500

    Tool tips for all fields

--- frontend/src/components/HitBoxDetail.tsx
+++ frontend/src/components/HitBoxDetail.tsx
@@ -1,8 +1,12 @@
 //React Imports
 import * as React from "react"
+import ReactTooltip from "react-tooltip";
 
 //CSS Imports
 import '../css/HitBoxDetail.css';
+
+//Import Data
+import * as hitboxFields from '../data/hitboxFields.json'
 
 //Media Imports
 import x_dark from '../media/darkmode/x.png'
@@ -24,11 +28,14 @@ function HitBoxDetail(props) {
     //Parse the hitbox json data so it is held in an array of 2 element arrays. Each inner array contains the key and the value
     let dataArrays = Object.entries(props.hitboxData);
     let displayData = [];
+    let infoToolTips = []
+
     //For each array within the dataArrays array, create a string based on the value and add it to displayData
     //Omit data where they key is 'frames', 'color', or 'notes', those are self inserted by myself and not part of the real game code
     dataArrays.forEach(pair => {
       if (pair[0] !== "frames" && pair[0] !== "color" && pair[0] !== "notes") {
-        displayData.push(<p><b>{pair[0]}</b>: {pair[1]}</p>)
+        displayData.push(<p><b key={pair[0]} data-tip data-for={pair[0]}>{pair[0]}</b>: {pair[1]}</p>)
+        infoToolTips.push(<ReactTooltip key={pair[0]} id={pair[0]} place="top" effect="solid">{hitboxFields[pair[0]]["toolTipDescription"]}</ReactTooltip>)
       }
     })
 
@@ -38,6 +45,7 @@ function HitBoxDetail(props) {
         <img id="exit" onClick={props.updateHitboxData.bind(this, undefined)} src={x[Math.abs(props.settings.dark_light-1)]}/>
 
         {displayData}
+        {infoToolTips}
       </div>
     )
   }
